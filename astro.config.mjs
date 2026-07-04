2c71d68eb4347239b465b3459aec70115bd41a62
emsacode <emsacode@gmail.com>
2026-07-04 16:25:38 +0700

    feat: integrate Google Tag Manager and Analytics, remove e-paper menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  site: 'https://perpustakaan.iainsorong.ac.id',
+  integrations: [tailwind(), mdx(), sitemap()]
 });
