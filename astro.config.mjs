ec645f84f7bab128f8b712ca3c2687d377ae3cf0
emsacode <emsacode@gmail.com>
2026-07-09 18:32:18 +0700

    Feat: Migrate to SSR architecture for dynamic content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://perpustakaan.iainsorong.ac.id',
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind(), mdx(), sitemap()]
 });
